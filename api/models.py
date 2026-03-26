b086abee6b6fd16bdd0fab13074d2717739cc535
joshdels <deleonjoshy2@gmail.com>
2026-03-26 14:16:08 +0800

    feat: update model with utility_type

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Project(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, blank=True)
     description = models.CharField(max_length=500, blank=True)
     logo = models.ImageField(upload_to="logos/", null=True, blank=True)
@@ -13,30 +13,45 @@ class Project(models.Model):
 
 class ProjectFile(models.Model):
     project = models.ForeignKey(
-        Project, on_delete=models.CASCADE, null=True, blank=True
+        Project,
+        on_delete=models.CASCADE,
     )
+    name = models.CharField(max_length=255, null=True, blank=True)
     file = models.FileField(upload_to="project_files/")
 
 
 class Asset(models.Model):
+    UTILITY_TYPE = [
+        ("water", "Water"),
+        ("electricity", "Electricity"),
+        ("irrigation", "Irrigation"),
+        ("sewerage", "Sewerage"),
+    ]
+
     ASSET_TYPE = [
         ("pipe", "Pipe"),
-        ("value", "Valve"),
+        ("valve", "Valve"),
         ("hydrant", "Hydrant"),
         ("pump", "Pump"),
+        ("tvalve", "T-Valve"),
     ]
 
     project = models.ForeignKey(
         Project, on_delete=models.CASCADE, null=True, blank=True
     )
+    utility_type = models.CharField(max_length=20, choices=UTILITY_TYPE)
     asset_type = models.CharField(max_length=20, choices=ASSET_TYPE)
     name = models.CharField(max_length=255, blank=True)
     status = models.CharField(max_length=50, default="active")
+    properties = models.JSONField(blank=True, null=True)
 
 
 class Node(models.Model):
-    asset = models.OneToOneField(Asset, on_delete=models.CASCADE, null=True, blank=True)
+    asset = models.OneToOneField(Asset, on_delete=models.CASCADE, related_name="node")
     geometry = models.PointField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Node: {self.asset.name if self.asset else 'Unnamed'}"
 
 
 class Pipe(models.Model):
@@ -54,3 +69,7 @@ class Pipe(models.Model):
     geometry = models.LineStringField(null=True, blank=True)
     diameter = models.FloatField(null=True, blank=True)
     material = models.CharField(max_length=50, blank=True)
+    properties = models.JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Pipe: {self.asset.name if self.asset else 'Unnamed'}"
